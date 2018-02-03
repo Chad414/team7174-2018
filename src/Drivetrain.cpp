8d7f375a51771bcb84d502cc6c5d9e0afd29e664
Rami Shams <shamsrami722@gmail.com>
2018-02-03 12:53:46 -0500

    Added DistancePIDWrapper

--- src/Drivetrain.cpp
+++ src/Drivetrain.cpp
@@ -10,13 +10,20 @@
 
 Drivetrain::Drivetrain()
 	: m_lDrive1(LEFT_DRIVE1),
-	m_lDrive2(LEFT_DRIVE2),
+	//m_lDrive2(LEFT_DRIVE2),
 	m_rDrive1(RIGHT_DRIVE1),
-	m_rDrive2(RIGHT_DRIVE2),
-	m_drive(m_lDrive1, m_lDrive2, m_rDrive1, m_rDrive2)
+	//m_rDrive2(RIGHT_DRIVE2),
+	m_drive(m_lDrive1, m_rDrive1),
+	m_lEncoder(LEFT_ENCODER, LEFT_ENCODER, false),
+	m_rEncoder(RIGHT_ENCODER, RIGHT_ENCODER, true)
 {
 	m_drive.SetSafetyEnabled(false);
+
+	m_turn = 0;
+	m_speed = 0;
 }
+
+// ---------- Drivetrain Functions ----------
 
 Drivetrain::~Drivetrain() {}
 
@@ -25,19 +32,52 @@ void Drivetrain::ArcadeDrive(double speed, double angle) {
 	m_drive.ArcadeDrive(speed, angle);
 }
 
-/*void Drivetrain::driveForward(double speed) {
-	if ((speed >= -1.0) && (speed <= 1.0)) {
-		m_lDrive1.Set(speed);
-		m_lDrive2.Set(speed);
-		m_rDrive1.Set(-speed);
-		m_rDrive2.Set(-speed);
-		std::cout << m_lDrive1.Get() << std::endl;
-	}
-}*/
+double Drivetrain::getRawEncoderValues() {
+	return ((m_lEncoder.Get() + m_rEncoder.Get()) / 2);
+}
+
+double Drivetrain::getAverageDistance() {
+	return ((getLeftEncoder() + getRightEncoder()) / 2);
+}
+
+double Drivetrain::getLeftEncoder() {
+	return m_lEncoder.Get() * DRIVE_ENCODER_CONVERSION;
+}
+
+double Drivetrain::getRightEncoder() {
+	return m_rEncoder.Get() * DRIVE_ENCODER_CONVERSION;
+}
 
 void Drivetrain::stop() {
 	m_lDrive1.StopMotor();
-	m_lDrive2.StopMotor();
+	//m_lDrive2.StopMotor();
 	m_rDrive1.StopMotor();
-	m_rDrive2.StopMotor();
+	//m_rDrive2.StopMotor();
 }
+
+void Drivetrain::setSpeed(double speed) {
+	ArcadeDrive(speed, m_turn);
+}
+
+void Drivetrain::setDistancePIDSpeed(double speed) {
+	distancePIDSpeed = speed;
+}
+
+// ---------- DistancePIDWrapper Functions ----------
+
+Drivetrain::DistancePIDWrapper::DistancePIDWrapper(Drivetrain* drivetrain) {
+	m_drivetrain = drivetrain;
+}
+
+Drivetrain::DistancePIDWrapper::~DistancePIDWrapper() {
+}
+
+double Drivetrain::DistancePIDWrapper::PIDGet () {
+	return(m_drivetrain->getAverageDistance() / DRIVE_ENCODER_CONVERSION);
+}
+
+void Drivetrain::DistancePIDWrapper::PIDWrite(double output) {
+	m_drivetrain->distancePIDOutput = output;
+	m_drivetrain->setSpeed(output * m_drivetrain->distancePIDSpeed);
+}
+
--- src/Drivetrain.h
+++ src/Drivetrain.h
@@ -12,10 +12,23 @@
 #ifndef SRC_DRIVETRAIN_H_
 #define SRC_DRIVETRAIN_H_
 
-#define LEFT_DRIVE1 0
-#define LEFT_DRIVE2 2
-#define RIGHT_DRIVE1 1
-#define RIGHT_DRIVE2 3
+#define DRIVE_ENCODER_CONVERSION 1.0 // Needs to be updated
+
+#define DISTANCE_P 0.5
+#define DISTANCE_I 0
+#define DISTANCE_D 0
+
+#define ANGLE_P 0.5
+#define ANGLE_I 0
+#define ANGLE_D 0.0
+
+#define LEFT_DRIVE1 14
+//#define LEFT_DRIVE2 2
+#define RIGHT_DRIVE1 15
+//#define RIGHT_DRIVE2 3
+
+#define LEFT_ENCODER 11 // Encoder values need to be updated
+#define RIGHT_ENCODER 12
 
 class Drivetrain {
 public:
@@ -25,13 +38,53 @@ public:
 	void ArcadeDrive(double speed, double angle);
 	void driveForward(double speed);
 	void stop();
+
+	double getRawEncoderValues();
+	double getLeftEncoder();
+	double getRightEncoder();
+	double getAverageDistance();
+	void setSpeed(double speed);
+	void setDistancePIDSpeed(double speed);
 private:
+
+	class DistancePIDWrapper : public PIDSource, public PIDOutput {
+	public:
+		DistancePIDWrapper(Drivetrain* drivetrain);
+		virtual ~DistancePIDWrapper();
+		double PIDGet() override;
+		void PIDWrite(double output) override;
+	private:
+		Drivetrain* m_drivetrain;
+	};
+
+	/*class AnglePIDWrapper : public PIDSource, public PIDOutput {
+	public:
+		AnglePIDWrapper(Drivetrain* drivetrain);
+		virtual ~AnglePIDWrapper();
+		double PIDGet() override;
+		void PIDWrite(double output) override;
+	private:
+		Drivetrain* m_drivetrain;
+	};*/
+
+	double m_speed;
+	double m_turn;
+
+	double distancePIDSpeed = 0.8;
+	double distancePIDOutput = 0.0;
+
 	WPI_TalonSRX m_lDrive1;
-	WPI_TalonSRX m_lDrive2;
+	//WPI_TalonSRX m_lDrive2;
 	WPI_TalonSRX m_rDrive1;
-	WPI_TalonSRX m_rDrive2;
+	//WPI_TalonSRX m_rDrive2;
 
 	RobotDrive m_drive;
+
+	Encoder m_lEncoder;
+	Encoder m_rEncoder;
+
+	//DistancePIDWrapper m_distancePIDWrapper;
+	//AnglePIDWrapper m_anglePIDWrapper;
 };
 
 #endif /* SRC_DRIVETRAIN_H_ */
